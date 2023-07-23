608e42b1642b9a42ce57704d4de347e2cce362cd
Danila Petrov <dp200308@gmail.com>
2023-07-23 12:43:26 +0500

    feat: add api realization

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,95 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+  allPass, compose, ifElse,
+  partialRight, pipe, prop,
+  tap, length, gt, test,
+  not, over, lensProp, curry, lt,
+  andThen, otherwise, isNil, identity
+} from "ramda";
+import {pipeAsync} from "ramda-async";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const getValue = prop('value')
+const getErrorHandler = prop('handleError')
+const logValue = tap(({value, writeLog}) => !isNil(value) ? writeLog(value) : null)
+const isLowerThan10 = compose(gt(10), length, getValue)
+const isGreaterThan2 = compose(lt(2), length, getValue)
+const isPositive = compose(lt(0), Number, getValue)
+const hasNotInvalidChars = compose(not, test(/[^.\d]/g), getValue)
+const throwValidationError = compose(handler => handler('Validation error'), getErrorHandler)
+const roundValue = compose(Math.floor, Number)
+const validations = allPass([
+    isLowerThan10,
+    isGreaterThan2,
+    isPositive,
+    hasNotInvalidChars
+])
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const request = curry((url, obj) => pipe(
+  () => api.get(url, {from: 10, to: 2, number: obj.value})
+)(obj))
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+// Flow functions
+const roundValueHandler = compose(logValue, over(lensProp('value'), roundValue))
+const requestBinaryHandler = obj => pipe(
+  request('https://api.tech/numbers/base'),
+  otherwise(compose(
+    () => ({result: undefined}),
+    obj.handleError
+  )),
+  andThen(({result}) => ({...obj, value: result})),
+  andThen(logValue)
+)(obj)
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+// Empty handlers
+const emptyValueHandler = ifElse(compose(isNil, getValue), identity)
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const requestAnimalHandler = emptyValueHandler(obj => pipe(
+  request(`https://animals.tech/${obj.value}`),
+  otherwise(compose(
+    obj.handleError
+  )),
+  andThen(({result}) => result),
+  andThen(obj.handleSuccess)
+)(obj))
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const valueLengthHandler = emptyValueHandler(compose(
+  logValue,
+  over(lensProp('value'), length),
+))
+
+const valuePowHandler = emptyValueHandler(compose(
+  logValue,
+  over(lensProp('value'), partialRight(Math.pow, [2])),
+))
+
+const valueModuloHandler = emptyValueHandler(compose(
+  logValue,
+  over(lensProp('value'), x => x % 3),
+))
+
+const runWorkflow = pipeAsync(
+  roundValueHandler,
+  requestBinaryHandler,
+  valueLengthHandler,
+  valuePowHandler,
+  valueModuloHandler,
+  requestAnimalHandler
+)
+
+// Entry point
+const processSequence = pipe(
+  logValue,
+  ifElse(
+    validations,
+    runWorkflow,
+    throwValidationError
+  ),
+)
+
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -12,16 +12,24 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
-import {allPass, compose, equals, prop} from "ramda";
+import {
+  allPass, any, anyPass,
+  compose, count, countBy,
+  equals, filter, identity,
+  length, lte, not, omit,
+  pick, prop, props, values
+} from "ramda";
 
 const isWhite = equals('white');
 const isRed = equals('red');
+const isBlue = equals('blue');
 const isGreen = equals('green');
 const isOrange = equals('orange');
 const getCircle = prop('circle')
 const getTriangle = prop('triangle')
 const getSquare = prop('square')
 const getStar = prop('star')
+const propsValues = props(['circle', 'triangle', 'square', 'star'])
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
 export const validateFieldN1 = allPass([
@@ -32,28 +40,85 @@ export const validateFieldN1 = allPass([
 ])
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = compose(
+  lte(2),
+  count(isGreen),
+  propsValues
+)
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = compose(
+  ([a, b]) => a === b,
+  values,
+  countBy(identity),
+  filter(anyPass([isRed, isBlue])),
+  propsValues
+)
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+  compose(isBlue, getCircle),
+  compose(isRed, getStar),
+  compose(isOrange, getSquare),
+])
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = compose(
+  any(lte(3)),
+  values,
+  omit(['white']),
+  countBy(identity),
+  propsValues
+)
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+  compose(isGreen, getTriangle),
+  compose(
+    equals(2),
+    length,
+    filter(isGreen),
+    propsValues
+  ),
+  compose(
+    lte(1),
+    length,
+    filter(isRed),
+    propsValues
+  )
+])
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = compose(
+  equals(4),
+  count(isOrange),
+  propsValues
+)
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = compose(
+  not,
+  anyPass([
+    compose(isRed, getStar),
+    compose(isWhite, getStar()),
+  ])
+)
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = allPass([
+  compose(isGreen, getStar),
+  compose(isGreen, getSquare),
+  compose(isGreen, getTriangle),
+  compose(isGreen, getCircle)
+])
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([
+  compose(not, isWhite, getTriangle),
+  compose(not, isWhite, getSquare),
+  compose(
+    ([a, b]) => a === b,
+    values,
+    pick(['triangle', 'square'])
+  )
+])
